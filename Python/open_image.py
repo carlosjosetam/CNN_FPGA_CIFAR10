a42d339743134235ff860108db266344e3fc73e0
Carlos J <carlosjosetam@gmail.com>
2018-11-23 09:37:32 +0100

    open-image

--- Python/open_image.py
+++ Python/open_image.py
@@ -8,9 +8,42 @@ def read_pgm(path):
     assert depth <= 255
 
     raster = []
-    for y in range(height):
+    for j in range(width):
         row = []
-        for y in range(width):
-            row.append(ord(f.read(1)))
+        line = f.readline().split()
+        print line
+        for i in range(height):
+            row.append(line[i])
         raster.append(row)
     return raster
+
+
+I = read_pgm("image_2.pgm");
+print(I)
+fh = 3
+fw = 3
+c = 1
+d = 1
+F = [[[[1 for k in xrange(fh)] for j in xrange(fw)] for i in xrange(d)] for i in xrange(c)]
+B = [1]
+#image_out = convolution([I,I], F, 1)
+
+# open file for writing
+filename = 'out.pgm'
+fout=open(filename, 'wb')
+
+# define PGM Header
+w = len(I)
+h = len(I[0])
+print "size image", h, w
+pgmHeader = 'P2' + ' ' + str(w) + ' ' + str(h) + ' ' + str(255) +  '\n'
+pgmHeader_byte = bytearray(pgmHeader,'utf-8')
+# write the header to the file
+fout.write(pgmHeader_byte)
+
+for j in range(w):
+    for i in range(h):
+        fout.write(str(I[j][i]))
+        fout.write("\n")
+
+fout.close()
